6e07f4f6118073fffaf0c494fd87b2ad3d6f32eb
horllarboiy <olamidetijani20@gmail.com>
2025-04-12 19:38:02 +0100

    feat: add AnimatedBox component for enhanced animations and integrate into multiple sections

---
+++ src/components/AnimatedBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { motion } from 'motion/react'
+import {useInView} from 'react-intersection-observer'
+
+const AnimatedBox = ({ children }) => {
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold:0.1
+    })
+  return (
+      <motion.div
+          ref={ref}
+          initial={{ opacity: 0, y: 40 }}
+          animate={inView?{ opacity: 1, y: 0 }: {}}
+          transition={{duration:0.7}}
+      >
+          {children}
+      </motion.div>
+  )
+}
+
+export default AnimatedBox

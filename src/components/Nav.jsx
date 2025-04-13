63833f459ef792a6bf709a4eac628f3fc67737ed
horllarboiy <olamidetijani20@gmail.com>
2025-04-13 21:12:24 +0100

    testing collapsible navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { CgClose } from "react-icons/cg";
+import { span } from "motion/react-client";
 
 const Nav = () => {
   const [menu, setMenu] = useState(false);
@@ -8,19 +9,132 @@ const Nav = () => {
     setMenu((prev) => !prev);
   };
   return (
-    <nav style={{ background: `#0E141C` }} className="text-white py-10">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl">Investor X</h1>
-        <ul className="md:flex hidden peer-checked:block text-xl">
-          <li className="mr-8">Home</li>
-          <li className="mr-8">About</li>
-          <li className="mr-8">Pages</li>
-          <li className="mr-8">Portfolio</li>
-          <li className="mr-8">Contact</li>
-        </ul>
-        <button onClick={handleClick} className="text-3xl transition-transform duration-150 ease-linear">
+    <nav className="bg-[#0E141C]">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-5">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Investor X
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-dropdown"
+          onClick={handleClick}
+          type="button"
+          className="inline-flex items-center p-2  justify-center text-3xl text-gray-300 rounded-lg md:hidden"
+          aria-controls="navbar-dropdown"
+          aria-expanded="false"
+        >
           {menu ? <CgClose /> : <HiMenuAlt4 />}
         </button>
+        <div className="w-full md:block md:w-auto" id="navbar-dropdown">
+          <ul
+            className={`${
+              menu ? `absolute` : `hidden`
+            } bg-[#0E141C] opacity-95 w-full left-0 md:w-auto text-xl md:flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0`}
+          >
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 "
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              >
+                Pages
+                <svg
+                  className="w-2.5 h-2.5 ms-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+
+              <div
+                id="dropdownNavbar"
+                className="z-10 absolute hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Earnings
+                    </a>
+                  </li>
+                </ul>
+                <div className="py-1">
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  >
+                    Sign out
+                  </a>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Portfolo
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );

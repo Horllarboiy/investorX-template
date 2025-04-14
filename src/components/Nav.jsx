5863bfcf19bbfb1139fad4affdb4d62fe497bbe9
horllarboiy <olamidetijani20@gmail.com>
2025-04-14 17:55:44 +0100

    testing some features

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { CgClose } from "react-icons/cg";
-import { span } from "motion/react-client";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 const Nav = () => {
   const [menu, setMenu] = useState(false);
+   const [dropDown, setDropDown] = useState(false);
   const handleClick = () => {
     setMenu((prev) => !prev);
+  };
+  const handleDropDown = () => {
+    setDropDown((prev) => !prev);
   };
   return (
     <nav className="bg-[#0E141C]">
@@ -34,51 +38,41 @@ const Nav = () => {
           >
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 "
-                aria-current="page"
               >
                 Home
               </a>
             </li>
             <li>
               <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                href="/about"
+                className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0"
               >
                 About
               </a>
             </li>
             <li>
               <button
+                onClick={handleDropDown}
                 id="dropdownNavbarLink"
                 data-dropdown-toggle="dropdownNavbar"
                 className="flex items-center w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
                 Pages
-                <svg
-                  className="w-2.5 h-2.5 ms-2.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
+                <span>
+                  {dropDown ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+                </span>
               </button>
 
               <div
                 id="dropdownNavbar"
-                className="z-10 absolute hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"
+                className={`z-40 md:absolute font-normal divide-y divide-gray-100 rounded-lg shadow-sm`}
               >
                 <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                  className={`py-2 text-sm ${
+                    dropDown ? `block` : `hidden`
+                  } text-gray-300 `}
                   aria-labelledby="dropdownLargeButton"
                 >
                   <li>
@@ -106,21 +100,13 @@ const Nav = () => {
                     </a>
                   </li>
                 </ul>
-                <div className="py-1">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Sign out
-                  </a>
-                </div>
               </div>
             </li>
 
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0"
               >
                 Portfolo
               </a>
@@ -128,7 +114,7 @@ const Nav = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0"
               >
                 Contact
               </a>
